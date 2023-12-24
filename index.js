1d95a3bc7c4cd1bdabd656832dd5b5575961fc8c
Zapperdax <qa80986@gmail.com>
2023-12-24 18:38:14 +0500

    time change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const CronJob = require("cron").CronJob;
 const {exec} = require("child_process");
 const bashScriptPath = './git_commit.sh';
-const job = new CronJob('35 18 * * *', () => {
+const job = new CronJob('39 18 * * *', () => {
   exec(`/bin/bash ${bashScriptPath}`, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error executing Bash script: ${error}`);
