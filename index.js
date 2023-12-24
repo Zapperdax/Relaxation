a49dc4d107a09ce16587dff93bd29cb900e8d4af
Zapperdax <qa80986@gmail.com>
2023-12-24 20:10:16 +0500

    vercel added

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const CronJob = require("cron").CronJob;
 const {exec} = require("child_process");
 const bashScriptPath = './git_commit.sh';
-console.log('JOB TRIGGERED');
-const job = new CronJob('23 19 * * *', () => {
-    console.log('JOB TRIGGERED');
-  exec(`which bash`, (error, stdout, stderr) => {
-    console.log(`bash path:${stdout}`);
+const job = new CronJob('6 19 * * *', () => {
+  exec(`bash ${bashScriptPath}`, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error executing Bash script: ${error}`);
       return;
