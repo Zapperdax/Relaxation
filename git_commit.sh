f5fe4b4e96a2199667225387ba32032c9547a4f0
Zapperdax <qa80986@gmail.com>
2023-12-24 17:56:48 +0500

    Added env vars

--- git_commit.sh
+++ git_commit.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 
+source "$(dirname "$0)/.env"
 # Your Git repository path
-repo_path="/home/zapperdax/Desktop/Codes/React/Commiting-REPO"
+repo_path="https://github.com/Zapperdax/Relaxation"
 
 # The file you want to edit
-file_to_edit="$repo_path/src/dailyTimeStamps.txt"
+file_to_edit="$repo_path/timestamps.txt"
 
+# Your GitHub branch
+branch="main"
 
-    # Generate a new commit message with the current date
-    commit_message="Some Changes"
+# Set Git credentials
+git config --global credential.helper store
+echo -e "https://${github_username}:${github_token}@github.com" > ~/.git-credentials
 
-    # Navigate to your Git repository directory
-    cd "$repo_path"
-    
-    # Generate a random number between 1 and 40
-    random_number=$((10 + RANDOM % 40))
+# Generate a new commit message with the current date
+commit_message="Some Changes"
 
-    # Loop for the generated random number of times
-    for ((i=1; i<=$random_number; i++))
-    do
-        # Make a change to the file (e.g., add a timestamp)
-        echo "Timestamp: $(date)" >> "$file_to_edit"
-	# Before and after Git commands, add lines to print debugging information
+# Navigate to your Git repository directory
+cd "$repo_path"
 
-        # Perform Git commands
-        git add .
-        git commit -m "$commit_message"
-        sleep $((10 + RANDOM % 100))
-    done
-    
-    git push -u origin main
-    
+# Generate a random number between 1 and 40
+random_number=$((10 + RANDOM % 5))
+
+# Loop for the generated random number of times
+for ((i=1; i<=$random_number; i++))
+do
+    # Make a change to the file (e.g., add a timestamp)
+    echo "Timestamp: $(date)" >> "$file_to_edit"
+
+    # Before and after Git commands, add lines to print debugging information
+
+    # Perform Git commands
+    git add .
+    git commit -m "$commit_message"
+    sleep $((10 + RANDOM % 100))
+done
+
+# Push to the specified branch in the remote repository
+git push -u origin "$branch"
 
