848be0e86de13f5efb11b26fde76e7b40c2f06ac
= <=>
2024-01-24 19:45:28 +0500

    Some Changes

--- git_commit.sh
+++ git_commit.sh
@@ -20,21 +20,16 @@ cd "$(dirname "$0")"
 sha=$(curl -s -H "Authorization: token ${github_token}" "https://api.github.com/repos/$repo/contents/$file_path?ref=$branch" | jq -r .sha)
 
 # Generate a random number between 1 and 5
-random_number=$((1 + RANDOM % 5))
 
 # Loop for the generated random number of times
-for ((i=1; i<=$random_number; i++))
-do
-    # Make a change to the file (e.g., add a timestamp)
-    echo "Timestamp: $(date)" >> "$file_path"
+  # Make a change to the file (e.g., add a timestamp)
+  echo "Timestamp: $(date)" >> "$file_path"
 
-    # Before and after Git commands, add lines to print debugging information
+  # Before and after Git commands, add lines to print debugging information
 
-    # Perform Git commands
-    git add .
-    git commit -m "$commit_message"
-    # sleep $((10 + RANDOM % 1000))
-done
+  # Perform Git commands
+  git add .
+  git commit -m "$commit_message"
 
 # Push to the specified branch in the remote repository
 git push -u origin "$branch"
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const CronJob = require("cron").CronJob;
 const { exec } = require("child_process");
 const path = require("path");
+
 const bashScriptPath = path.join(__dirname, "git_commit.sh");
 
+const generateRandomTime = () => {
+  // const hour = Math.floor(Math.random() * 24);
+  // const minute = Math.floor(Math.random() * 60);
+  const hour = 19;
+  const minute = 35;
+  return `${minute} ${hour} * * *`; // Format: minute hour * * *
+};
 
 const runScheduledJob = () => {
   exec(`/bin/bash ${bashScriptPath}`, (error, stdout, stderr) => {
@@ -15,8 +23,22 @@ const runScheduledJob = () => {
   });
 };
 
-// Imagine it not working
-const job = new CronJob('32 20 * * *', runScheduledJob, null, true, 'Asia/Karachi');
-job.start();
+const startJobScheduler = () => {
+    //const loop = () => {
+    //const randomTimeExpression = generateRandomTime();
+    //const job = new CronJob(randomTimeExpression, runScheduledJob, null, false, 'Asia/Karachi');
+    //job.start();
 
-module.exports = runScheduledJob;
+    // Wait for the next execution
+    //setTimeout(() => {
+      //job.stop();
+     // loop();
+    //}, 24 * 60 * 60 * 1000); // Wait for 24 hours before starting the loop again
+  //};
+
+  //loop();
+  runScheduledJob();
+};
+
+// Start the job scheduler
+startJobScheduler();
